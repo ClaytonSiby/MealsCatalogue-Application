ad1c9aed82a5f77527a9f43d5127d3bc0a6086ba
ClaytonSiby <claytonsiby@gmail.com>
2021-03-25 09:36:26 +0200

    Implement the rootReducer function & initialize the redux store

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import foodReducer from './food/foodReducer';
+import categoriesReducer from './categories/categoriesReducer';
+import foodListReducer from './foodlist/foodlistReducer';
+
+const rootReducer = combineReducers({
+    food: foodReducer,
+    categories: categoriesReducer,
+    foodlist: foodListReducer
+})
+
+export default rootReducer;
