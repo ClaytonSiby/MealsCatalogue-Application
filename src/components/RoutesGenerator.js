ad24a4fe86a16c32698adc2180fdfdc059ce4e52
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 10:32:17 +0200

    Update the RoutesGenerator component to include a Switch component to it

--- src/components/RoutesGenerator.js
+++ src/components/RoutesGenerator.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import App from './App';
-import CategoryDetails from './CategoryDetails';
-import FoodDetails from './FoodDetails';
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import App from './App'
+import CategoryDetails from './CategoryDetails'
+import FoodDetails from './FoodDetails'
 
-const history = createBrowserHistory();
+const history = createBrowserHistory()
 
 const Routes = () => (
   <Router history={history}>
-    <Route exact path="/" component={App} />
-    <Route exact path="/categories/:strCategory" component={CategoryDetails} />
-    <Route exact path="/food/:foodId" component={FoodDetails} />
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route
+        exact
+        path='/categories/:strCategory'
+        component={CategoryDetails}
+      />
+      <Route exact path='/food/:foodId' component={FoodDetails} />
+    </Switch>
   </Router>
-);
+)
 
-export default Routes;
+export default Routes
