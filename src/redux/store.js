d68b42d30a6e4304f372268e6217b4cb44f6f004
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 10:34:38 +0200

    Update redux store to & store it in the localStorage for better performance

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,31 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
+const saveToLocalStorage = state => {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem('applicationData', serialisedState);
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+const loadAppData = () => {
+  try {
+    const serialisedState = localStorage.getItem('applicationData');
+    if(serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch(error) {
+    console.warn(error)
+    return undefined
+  }
+}
+
 const store = createStore(
-  rootReducer,
+  rootReducer, loadAppData(),
   composeWithDevTools(applyMiddleware(thunk, logger)),
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store;
