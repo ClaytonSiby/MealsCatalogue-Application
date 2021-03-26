6f0ca1a693117979aa924b5b4e285b1023178104
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 10:33:57 +0200

    Update the rootReducer to take in the itemDetails property

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import foodReducer from './food/foodReducer';
 import categoriesReducer from './categories/categoriesReducer';
 import foodListReducer from './foodlist/foodlistReducer';
+import itemDetailsReducer from './itemDetailsReducer';
 
 const rootReducer = combineReducers({
   food: foodReducer,
   categoriesData: categoriesReducer,
   foodlist: foodListReducer,
+  itemDetails: itemDetailsReducer
 });
 
 export default rootReducer;
