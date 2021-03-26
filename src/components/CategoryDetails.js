e3062f9c3db9b8eeb00ade7da09b98c24e8ac381
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 11:19:11 +0200

    Structure food items from the CategoryDetails page

--- src/components/CategoryDetails.js
+++ src/components/CategoryDetails.js
@@ -1,34 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import FOODLISTDATAFETCH from '../redux/foodlist/foodlistActions';
-import LoadingComponent from './LoadingComponent';
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import FOODLISTDATAFETCH from '../redux/foodlist/foodlistActions'
+import LoadingComponent from './LoadingComponent'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 const CategoryDetails = () => {
-  const dispatch = useDispatch();
-  const category = useSelector((state) => state.itemDetails.categoryName);
-  const foodList = useSelector((state) => state.foodlist);
-  const [completed, setCompleted] = useState(false);
+  const dispatch = useDispatch()
+  const category = useSelector(state => state.itemDetails.categoryName)
+  const food = useSelector(state => state.foodlist.foodList)
+  const [completed, setCompleted] = useState(false)
   useEffect(() => {
     setTimeout(() => {
-      (async () => {
-        await dispatch(FOODLISTDATAFETCH.fetchFoodList(category));
-        setCompleted(true);
-      })();
-    }, 2000);
-  }, []);
+      ;(async () => {
+        await dispatch(FOODLISTDATAFETCH.fetchFoodList(category))
+        setCompleted(true)
+      })()
+    }, 2000)
+  }, [])
 
-  return !completed ? (
-    <div>
-      <h4>...loading</h4>
-      <LoadingComponent type="bars" color="#03cf4e" />
-    </div>
+  return (
+    <Container>
+      
+        {!completed ? (
+          <div>
+            <h4>...loading</h4>
+            <LoadingComponent type='bars' color='#03cf4e' />
+          </div>
+        ) : (
+          <Row>
+            {food &&
+              food.meals &&
+              food.meals.map(food => (
+                <Col xs={12} sm={6} md={4} key={food.idMeal} className="my-2">
+                  <Card>
+                    <Card.Body>
+                      <Card.Img variant='top' src={`${food.strMealThumb}`} />
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+          </Row>
+        )}
+      
+    </Container>
+  )
+}
 
-  ) : (
-    <div>
-      <p>Displaying all Data</p>
-      {JSON.stringify(foodList)}
-    </div>
-  );
-};
-
-export default CategoryDetails;
+export default CategoryDetails
