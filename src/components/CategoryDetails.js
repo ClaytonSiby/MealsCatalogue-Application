a8747441394b100f4f05197da76b95a840d32878
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 11:31:32 +0200

    Style the categoryDetails page & add a button to view more details on each food item

--- src/components/CategoryDetails.js
+++ src/components/CategoryDetails.js
@@ -20,28 +20,37 @@ const CategoryDetails = () => {
 
   return (
     <Container>
-      
-        {!completed ? (
-          <div>
-            <h4>...loading</h4>
-            <LoadingComponent type='bars' color='#03cf4e' />
-          </div>
-        ) : (
-          <Row>
-            {food &&
-              food.meals &&
-              food.meals.map(food => (
-                <Col xs={12} sm={6} md={4} key={food.idMeal} className="my-2">
-                  <Card>
-                    <Card.Body>
-                      <Card.Img variant='top' src={`${food.strMealThumb}`} />
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-          </Row>
-        )}
-      
+      {!completed ? (
+        <div
+          className='d-flex justify-content-center align-items-center'
+          style={{ height: '100vh', flexDirection: 'column' }}
+        >
+          <h4>...loading</h4>
+          <LoadingComponent type='bars' color='#03cf4e' />
+        </div>
+      ) : (
+        <Row>
+          {food &&
+            food.meals &&
+            food.meals.map(food => (
+              <Col xs={12} sm={6} md={3} key={food.idMeal} className='my-2'>
+                <Card className='m-0 p-0'>
+                  <Card.Img
+                    variant='top'
+                    alt='foodItem image'
+                    src={`${food.strMealThumb}`}
+                  />
+                  <Card.Body className='m-0 p-1'>
+                    <p>{food.strMeal}</p>
+                    <button type='button' className='btn btn-block btn-primary'>
+                      Meal Details
+                    </button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      )}
     </Container>
   )
 }
