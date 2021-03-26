5f1f1f1e501ea78d370ce217d5f64e5978d2f247
ClaytonSiby <claytonsiby@gmail.com>
2021-03-26 15:51:37 +0200

    Auto fix code errors with eslint

--- src/components/CategoryDetails.js
+++ src/components/CategoryDetails.js
@@ -1,57 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import FOODLISTDATAFETCH from '../redux/foodlist/foodlistActions'
-import LoadingComponent from './LoadingComponent'
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import { changeFoodId } from '../redux/itemDetailsReducer'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Container, Row, Col, Card,
+} from 'react-bootstrap';
+import FOODLISTDATAFETCH from '../redux/foodlist/foodlistActions';
+import LoadingComponent from './LoadingComponent';
+import { changeFoodId } from '../redux/itemDetailsReducer';
 import truncate from '../helpers/truncate';
 
 const CategoryDetails = () => {
-  const dispatch = useDispatch()
-  const category = useSelector(state => state.itemDetails.categoryName)
-  const food = useSelector(state => state.foodlist.foodList)
-  const [completed, setCompleted] = useState(false)
+  const dispatch = useDispatch();
+  const category = useSelector((state) => state.itemDetails.categoryName);
+  const food = useSelector((state) => state.foodlist.foodList);
+  const [completed, setCompleted] = useState(false);
   useEffect(() => {
     setTimeout(() => {
-      ;(async () => {
-        await dispatch(FOODLISTDATAFETCH.fetchFoodList(category))
-        setCompleted(true)
-      })()
-    }, 2000)
-  }, [])
+      (async () => {
+        await dispatch(FOODLISTDATAFETCH.fetchFoodList(category));
+        setCompleted(true);
+      })();
+    }, 2000);
+  }, []);
 
   return (
     <Container>
       {!completed ? (
         <div
-          className='d-flex justify-content-center align-items-center'
+          className="d-flex justify-content-center align-items-center"
           style={{ height: '100vh', flexDirection: 'column' }}
         >
           <h4>...loading</h4>
-          <LoadingComponent type='bars' color='#03cf4e' />
+          <LoadingComponent type="bars" color="#03cf4e" />
         </div>
       ) : (
         <Row>
-          {food &&
-            food.meals &&
-            food.meals.map(food => (
-              <Col xs={12} sm={6} md={3} key={food.idMeal} className='my-2'>
-                <Card className='m-0 p-0'>
+          {food
+            && food.meals
+            && food.meals.map((food) => (
+              <Col xs={12} sm={6} md={3} key={food.idMeal} className="my-2">
+                <Card className="m-0 p-0">
                   <Card.Img
-                    variant='top'
-                    alt='foodItem image'
+                    variant="top"
+                    alt="foodItem image"
                     src={`${food.strMealThumb}`}
                   />
-                  <Card.Body className='m-0 p-1'>
+                  <Card.Body className="m-0 p-1">
                     <p>{truncate(food.strMeal, 25)}</p>
                     <button
-                      type='button'
-                      className='btn btn-block btn-primary'
+                      type="button"
+                      className="btn btn-block btn-primary"
                       onClick={() => dispatch(changeFoodId(food.idMeal))}
                     >
                       <a
                         href={`\\food\\${food.idMeal}`}
-                        className='text-light'
+                        className="text-light"
                       >
                         Prepare Meal
                       </a>
@@ -63,7 +65,7 @@ const CategoryDetails = () => {
         </Row>
       )}
     </Container>
-  )
-}
+  );
+};
 
-export default CategoryDetails
+export default CategoryDetails;
--- src/components/FoodDetails.js
+++ src/components/FoodDetails.js
@@ -1,43 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import FOODDATAFETCH from '../redux/food/foodActions'
-import { connect } from 'react-redux'
-import LoadingComponent from './LoadingComponent';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import FOODDATAFETCH from '../redux/food/foodActions';
+import LoadingComponent from './LoadingComponent';
 
 const FoodDetails = ({ food, foodId, foodRequest }) => {
-  console.log(foodId)
-  const [doneLoading, setDoneLoading] = useState(false)
+  console.log(foodId);
+  const [doneLoading, setDoneLoading] = useState(false);
   useEffect(() => {
-    ;(async () => {
-      await foodRequest(foodId)
-      setDoneLoading(true)
-    })()
-  }, [])
+    (async () => {
+      await foodRequest(foodId);
+      setDoneLoading(true);
+    })();
+  }, []);
 
   return !doneLoading ? (
-    <LoadingComponent type='bars' color='#03cf4e' />
+    <LoadingComponent type="bars" color="#03cf4e" />
   ) : (
     <div>{JSON.stringify(food)}</div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => {
-  return {
-    food: state.food,
-    foodId: state.itemDetails.foodId
-  }
-}
+const mapStateToProps = (state) => ({
+  food: state.food,
+  foodId: state.itemDetails.foodId,
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    foodRequest: url => dispatch(FOODDATAFETCH.foodRequest(url))
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  foodRequest: (url) => dispatch(FOODDATAFETCH.foodRequest(url)),
+});
 
 FoodDetails.propTypes = {
   food: PropTypes.object.isRequired,
   foodRequest: PropTypes.func.isRequired,
-  foodId: PropTypes.string.isRequired
-}
+  foodId: PropTypes.string.isRequired,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(FoodDetails)
+export default connect(mapStateToProps, mapDispatchToProps)(FoodDetails);
