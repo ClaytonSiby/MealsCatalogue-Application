3fe64eac74f8be4056211a8e0172ca948e2602de
ClaytonSiby <claytonsiby@gmail.com>
2021-03-28 17:53:38 +0200

    Refactor the NavigationBar & use a loop to display each nav-item

--- src/containers/NavigationBar.js
+++ src/containers/NavigationBar.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { changeCategoryName } from '../redux/itemDetailsReducer';
 
 const NavigationBar = () => {
+  const [CATEGORIES] = useState([
+    'Breakfast',
+    'Dessert',
+    'Starter',
+    'Pasta',
+  ]);
+  const [DROPDOWNCATEGORIES] = useState([
+    'Beef',
+    'Chicken',
+    'Pork',
+    'Miscellaneous',
+    'Lamb',
+    'Seafood',
+    'Side',
+    'Vegan',
+    'Vegetarian',
+    'Goat',
+  ]);
   const dispatch = useDispatch();
   return (
     <Navbar sticky="top" bg="dark" expand="lg" className="my-3" variant="dark">
@@ -13,92 +31,27 @@ const NavigationBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link
-            onClick={() => dispatch(changeCategoryName('Breakfast'))}
-            href={'\\categories\\Breakfast'}
-          >
-            Breakfast
-          </Nav.Link>
-          <Nav.Link
-            onClick={() => dispatch(changeCategoryName('Dessert'))}
-            href={'\\categories\\Dessert'}
-          >
-            Dessert
-          </Nav.Link>
-          <Nav.Link
-            onClick={() => dispatch(changeCategoryName('Starter'))}
-            href={'\\categories\\Starter'}
-          >
-            Starter
-          </Nav.Link>
-          <Nav.Link
-            onClick={() => dispatch(changeCategoryName('Pasta'))}
-            href={'\\categories\\Pasta'}
-          >
-            Pasta
-          </Nav.Link>
+          {CATEGORIES.map((category) => (
+            <Nav.Link
+              key={Math.floor(Math.random() * 100)}
+              onClick={() => dispatch(changeCategoryName(category))}
+              href={`\\categories\\${category}`}
+            >
+              {category}
+            </Nav.Link>
+          ))}
           <NavDropdown title="Filter by Category" id="basic-nav-dropdown">
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Beef'))}
-              href={'\\categories\\Beef'}
-            >
-              Beef
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Chicken'))}
-              href={'\\categories\\Chicken'}
-            >
-              Chicken
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Pork'))}
-              href={'\\categories\\Pork'}
-            >
-              Pork
-            </NavDropdown.Item>
-
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Miscellaneous'))}
-              href={'\\categories\\Miscellaneous'}
-            >
-              Miscellaneous
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Lamb'))}
-              href={'\\categories\\Lamb'}
-            >
-              Lamb
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Seafood'))}
-              href={'\\categories\\Seafood'}
-            >
-              Seafood
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Side'))}
-              href={'\\categories\\Side'}
-            >
-              Side
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Vegan'))}
-              href={'\\categories\\Vegan'}
-            >
-              Vegan
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Vegetarian'))}
-              href={'\\categories\\Vegetarian'}
-            >
-              Vegetarian
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={() => dispatch(changeCategoryName('Goat'))}
-              href={'\\categories\\Goat'}
-            >
-              Goat
-            </NavDropdown.Item>
+            {DROPDOWNCATEGORIES.map((category) => (
+              <NavDropdown.Item
+                key={Math.floor(Math.random() * 100)}
+                onClick={() => dispatch(changeCategoryName(category))}
+                href={`\\categories\\${category}`}
+              >
+                {' '}
+                {category}
+                {' '}
+              </NavDropdown.Item>
+            ))}
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
