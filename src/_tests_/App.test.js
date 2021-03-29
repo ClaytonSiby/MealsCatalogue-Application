52e06e5a15442e867e71bd3877781e620661668b
ClaytonSiby <claytonsiby@gmail.com>
2021-03-29 20:24:48 +0200

    Add test suite for the App component

---
+++ src/_tests_/App.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import App from '../components/App'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+describe('With React Testing Library', () => {
+  const initialState = {
+    categoriesData: { loading: false, categories: {}, error: '' },
+    foodItem: { loading: false, food: {}, error: '' },
+    foodlist: { loading: false, foodList: [], error: '' },
+    itemDetails: { categoryName: '', foodId: '' }
+  }
+  const mockStore = configureStore()
+  let store = mockStore(initialState)
+
+  it('Renders successfully to the DOM', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  })
+
+  test('renders output consistently to the DOM', () => {
+    const test = renderer
+      .create(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+      .toJSON()
+    expect(test).toMatchSnapshot()
+  })
+})
