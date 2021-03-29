92674b71685473dbccb0d2233189eb7fb84dbe07
ClaytonSiby <claytonsiby@gmail.com>
2021-03-29 20:51:43 +0200

    Run eslint & stylelint auto error fix

--- src/_tests_/LoadingComponet.test.js
+++ src/_tests_/LoadingComponet.test.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import renderer from 'react-test-renderer'
-import LoadingComponent from '../components/LoadingComponent'
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import LoadingComponent from '../components/LoadingComponent';
 
 describe('Testing the About component', () => {
   it('Renders successfully to the DOM', () => {
-    render(<LoadingComponent type="bars" color="yellow" />)
-  })
+    render(<LoadingComponent type="bars" color="yellow" />);
+  });
 
   test('renders output consistently to the DOM', () => {
-    const test = renderer.create(<LoadingComponent type="bars" color="yellow" />).toJSON()
-    expect(test).toMatchSnapshot()
-  })
-})
+    const test = renderer.create(<LoadingComponent type="bars" color="yellow" />).toJSON();
+    expect(test).toMatchSnapshot();
+  });
+});
